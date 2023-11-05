43604aef6f92ca407ad15174e8f6837b455ec18a
arshad ali <arshad300390@gmail.com>
2023-11-06 00:12:45 +0500

    compelte date and time picker functionality

--- App.js
+++ App.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View , } from 'react-native';
-import { PaperProvider, } from 'react-native-paper';
+import {
+  StyleSheet, Text, View,
+} from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import BottomNavigationBar from './navigation/BottomNavigationBar';
 import CountryPickerForm from './CountryPickerForm';
 import DatePickerForm from './DatePickerForm';
 
 export default function App() {
- 
   return (
-    
     <PaperProvider>
       <View style={styles.container}>
         <CountryPickerForm />
-        
-        <Text>Multi Step  erForm</Text>
-       <DatePickerForm />
+        <Text>Multi Step Form</Text>
+        <DatePickerForm />
       </View>
       
-      <BottomNavigationBar />
-      </PaperProvider>
+        <BottomNavigationBar />
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop:100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
